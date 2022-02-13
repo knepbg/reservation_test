dfb6a0cf6f5b9817da0b872d6c67be5a0bc70a18
knepbg <knep.87@gmail.com>
2022-02-13 11:31:50 +0200

    first commit

---
+++ src/test/java/com/moonlight/roomreservations/RoomReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonlight.roomreservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
